b772c133584bd745ed52d64121a49e0ea748bc39
Daniel H <danny.huly@gmail.com>
2018-11-16 23:28:22 +0200

    Updates

---
+++ precache-manifest.4315bdc9a1a9e4351fd5e8b8b2d1470e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9f39852713b79fbebfba",
+    "url": "/gh-pages-react/static/js/runtime~main.9f398527.js"
+  },
+  {
+    "revision": "391697528ba526e42c68",
+    "url": "/gh-pages-react/static/js/main.39169752.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/gh-pages-react/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "391697528ba526e42c68",
+    "url": "/gh-pages-react/static/css/main.c34e983a.chunk.css"
+  },
+  {
+    "revision": "4c6e1b10985e73c3ca10f98a871148d2",
+    "url": "/gh-pages-react/index.html"
+  }
+];
