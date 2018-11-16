6b5dbc088ccf73d98d28c3840523066d0e45a643
Daniel H <danny.huly@gmail.com>
2018-11-16 23:59:48 +0200

    Add material-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              GitHub Pages React - POC
+          </Typography>
+          </Toolbar>
+        </AppBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
+          <Button variant="contained"
+            color="primary"
             href="https://reactjs.org"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             Learn React
-          </a>
+          </Button>
         </header>
       </div>
     );
